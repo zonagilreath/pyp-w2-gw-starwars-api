985e1826984d13e3e24be6898bd96a4f82bf7494
jsymons <jason.symons@gmail.com>
2017-07-26 19:19:18 -0400

    Add films tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -27,6 +27,20 @@ class PeopleTestCase(BaseStarWarsAPITestCase):
         with self.assertRaisesRegexp(SWAPIClientError, error):
             People.get(100)
 
+class FilmsTestCase(BaseStarWarsAPITestCase):
+    @responses.activate
+    def test_films_model(self):
+        new_hope = Films.get(1)
+        self.assertTrue(new_hope.title, 'A New Hope')
+        self.assertTrue(new_hope.director, 'George Lucas')
+        self.assertTrue(new_hope.release_date, '1977-05-25')
+
+    @responses.activate
+    def test_films_model_not_found(self):
+        error = ('Request to SWAPI "/api/films/100" failed with '
+                 'status "404". Reason: {"detail": "Not found"}')
+        with self.assertRaisesRegexp(SWAPIClientError, error):
+            Films.get(100)
 
 class PeopleQuerySetTestCase(BaseStarWarsAPITestCase):
 
@@ -46,3 +60,22 @@ class PeopleQuerySetTestCase(BaseStarWarsAPITestCase):
     def test_people_qs_count(self):
         qs = People.all()
         self.assertEqual(qs.count(), 15)
+
+class FilmsQuerySetTestCase(BaseStarWarsAPITestCase):
+
+    @responses.activate
+    def test_films_qs_next(self):
+        qs = Films.all()
+        obj = qs.next
+        self.assertTrue(isinstance(obj, Films))
+        self.assertEqual(obj.title, 'A New Hope')
+
+    @responses.activate
+    def test_films_qs_iterable(self):
+        qs = Films.all()
+        self.assertEqual(len([elem for elem in qs]), 7)
+
+    @responses.activate
+    def test_films_qs_count(self):
+        qs = Films.all()
+        self.assertEqual(qs.count(), 7)
